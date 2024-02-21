b95d7ce9b22b96461c6c3295d7b04d65b672792a
bygabymora <bygabymora@gmail.com>
2024-02-21 16:18:05 -0500

    Eliminar componente de video innecesario

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import RootLayout from '../components/layout';
 import Link from 'next/link';
-import VideoContainer from '../components/VideoContainer';
+
 import Image from 'next/image';
 import Boton from '../public/images/boton.png';
 
@@ -10,8 +10,6 @@ export default function Home() {
     <RootLayout title="Propuesta Comercial 2024">
       <main className="bg-gray-50 pt-2">
         <div className="justify-center items-center w-full pt-2 pb-2 leading-tight">
-          <VideoContainer />
-
           {/* Button container */}
           <div className=" p-2 flex justify-center items-center sm:relative sm:bottom-auto sm:w-auto sm:bg-transparent hide-on-mobile-landscape">
             <Link href="/formulario">
