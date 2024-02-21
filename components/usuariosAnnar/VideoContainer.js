63378fe8ba175712e745089750dd50bb897fca1c
bygabymora <bygabymora@gmail.com>
2024-02-21 16:29:30 -0500

    Eliminar archivo next.config.mjs

---
+++ components/usuariosAnnar/VideoContainer.js
@@ -0,0 +1,102 @@
+import React, { useEffect, useRef, useState } from 'react';
+import RootLayout from '../../components/layout';
+import Image from 'next/image';
+import { FaCirclePlay } from 'react-icons/fa6';
+
+const VideoContainer = () => {
+  const videoRef = useRef(null);
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  const togglePlay = () => {
+    if (videoRef.current) {
+      if (isPlaying) {
+        videoRef.current.pause();
+      } else {
+        videoRef.current
+          .play()
+          .then(() => {
+            requestFullScreen(videoRef.current);
+          })
+          .catch((err) => {
+            console.error('Error trying to play the video: ', err);
+          });
+      }
+    }
+  };
+
+  const requestFullScreen = (element) => {
+    if (element.requestFullscreen) {
+      element.requestFullscreen().catch((err) => {
+        console.error('Error trying to enable fullscreen: ', err);
+      });
+    } else if (element.webkitRequestFullscreen) {
+      /* Safari */
+      element.webkitRequestFullscreen().catch((err) => {
+        console.error('Error trying to enable fullscreen on Safari: ', err);
+      });
+    } else if (element.msRequestFullscreen) {
+      /* IE11 */
+      element.msRequestFullscreen().catch((err) => {
+        console.error('Error trying to enable fullscreen on IE11: ', err);
+      });
+    }
+  };
+
+  useEffect(() => {
+    const videoElement = videoRef.current;
+
+    if (videoElement) {
+      const handlePlay = () => setIsPlaying(true);
+      const handlePause = () => setIsPlaying(false);
+
+      videoElement.addEventListener('play', handlePlay);
+      videoElement.addEventListener('pause', handlePause);
+
+      // Optional: Attempt to autoplay the video on load
+      videoElement.play().catch((err) => {
+        // Handle the case where autoplay was prevented by the browser
+        console.log('Autoplay was prevented', err);
+      });
+
+      return () => {
+        videoElement.removeEventListener('play', handlePlay);
+        videoElement.removeEventListener('pause', handlePause);
+      };
+    }
+  }, []);
+
+  return (
+    <RootLayout title="Propuesta Comercial 2024">
+      <main className="flex flex-col items-center justify-center h-auto md:h-[75vh] lg:h-[80vh] mt-2 md:mt-5">
+        <div className="flex justify-center items-center w-full leading-tight sticky top-0 sm:hidden">
+          <Image
+            src="https://res.cloudinary.com/do6oloxvt/image/upload/v1706815543/VISI%C3%93N_2024_10_lxucse.png"
+            alt="Moraequipos"
+            className="rounded-t-lg w-[90%] mb-5 "
+            width={600}
+            height={320}
+          />
+        </div>
+        <div className="flex justify-center items-center leading-tight max-w-xl mx-auto w-full pt-2 px-2 relative lg:max-w-4xl">
+          <video
+            ref={videoRef}
+            id="propuestaComercial2024"
+            className="2xl:w-full h-auto rounded-lg shadow-xl w-[90%] lg:w-[80%]"
+            volume="0.8"
+            controls
+          >
+            <source src="/images/PropuestaComercial.mp4" type="video/mp4" />
+          </video>
+          {!isPlaying && (
+            <FaCirclePlay
+              className="absolute top-1/2 left-1/2 hidden md:block transform -translate-x-1/2 -translate-y-1/2 text-[10rem] text-[#8b1414] cursor-pointer opacity-70"
+              onClick={togglePlay}
+            />
+          )}
+        </div>
+      </main>
+    </RootLayout>
+  );
+};
+
+export default VideoContainer;
---
+++ pages/usuariosAnnar/index.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import RootLayout from '../../components/layout';
+import Link from 'next/link';
+import VideoContainer from '../../components/usuariosAnnar/VideoContainer';
+import Image from 'next/image';
+import Boton from '../../public/images/boton.png';
+
+export default function usuariosAnnar() {
+  return (
+    <RootLayout title="Propuesta Comercial 2024">
+      <main className="bg-gray-50 pt-2">
+        <div className="justify-center items-center w-full pt-2 pb-2 leading-tight">
+          <VideoContainer />
+
+          {/* Button container */}
+          <div className=" p-2 flex justify-center items-center sm:relative sm:bottom-auto sm:w-auto sm:bg-transparent hide-on-mobile-landscape">
+            <Link href="/formulario">
+              <Image
+                src={Boton}
+                alt="Cuéntenos sus expectativas."
+                className="w-[300px] "
+              />
+            </Link>
+            <div className="fixed bottom-0 left-0 w-full flex justify-center items-center leading-tight sm:hidden">
+              <Image
+                src="https://res.cloudinary.com/do6oloxvt/image/upload/v1706816338/Dise%C3%B1o_sin_t%C3%ADtulo_2_pow0h7.png"
+                alt="Moraequipos"
+                className="rounded-b-lg w-[90%] mb-5 "
+                width={600}
+                height={320}
+              />
+            </div>
+          </div>
+        </div>
+      </main>
+    </RootLayout>
+  );
+}
